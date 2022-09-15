ad295349ecf4e62f7188e13c9fe4be71e6a1c591
Anuraag Agrawal <anuraaga@gmail.com>
2022-09-15 14:09:04 +0900

    Use unsafe.Slice instead of SliceHeader (#3)
    Signed-off-by: Anuraag Agrawal <anuraaga@gmail.com>

--- tinymem.go
+++ tinymem.go
@@ -2,22 +2,20 @@
 //
 // Note: If you are only interested in exporting functions to the host, blank
 // import this package like so:
+//
 //	import _ github.com/tetratelabs/tinymem
 package tinymem
 
 import (
 	"unsafe"
-
-	"github.com/tetratelabs/tinymem/internal"
 )
 
 // PtrToString returns a string from WebAssembly compatible numeric types
 // representing its pointer and length.
 func PtrToString(ptr uintptr, size uint32) string {
-	// Get a slice view of the underlying bytes in the stream. We use
-	// SliceHeader, not StringHeader as it allows us to fix the capacity to
-	// what was allocated.
-	return *(*string)(unsafe.Pointer(internal.SliceHeader(ptr, size)))
+	// Get a slice view of the underlying bytes in the stream.
+	s := unsafe.Slice((*byte)(unsafe.Pointer(ptr)), size)
+	return *(*string)(unsafe.Pointer(&s))
 }
 
 // StringToPtr returns a pointer and size pair for the given string in a way
